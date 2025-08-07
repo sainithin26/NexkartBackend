ede706e34e8aaf7a7cdafd163a3cabd95b1c950e
sainithin26 <psnithin2003@gmail.com>
2025-08-07 06:30:23 +0100

    new backend services

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ app.use((error, req, res, next) => {
 });
 
 
-app.listen(process.env.PORT,'0.0.0.0', () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
